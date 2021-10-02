85ae57bae95e5fc49c3bf9412d729d090e945e77
Jai3181 <jaykumar9246@gmail.com>
2021-10-02 17:45:50 +0530

    user model and signup route created

---
+++ src/index.server.js
@@ -0,0 +1,32 @@
+// imports
+const express = require("express");
+const env = require("dotenv");
+const app = express()
+const bodyParser = require("body-parser")
+const mongoose = require('mongoose');
+
+//mongoose connect
+mongoose.connect(`mongodb+srv://root:admin@cluster0.pd2lx.mongodb.net/flipkart?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true
+  })
+  .then(() => {
+    console.log(`DATABASE CONNECT to ${process.env.MONGO_DB_USER} ${process.env.MONGO_DB_PASSWORD} ${process.env.MONGO_DB_DATABASE}`)
+  });
+
+//using variables
+env.config()
+
+//routes
+const userRoutes = require("./routes/user")
+
+//middlewares
+app.use(express.json())
+app.use("/api", userRoutes)
+
+//listener
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
